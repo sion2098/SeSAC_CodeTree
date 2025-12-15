2910b340b23a33431df0ab23dbfd67241180bf47
sion2098 <sioning2098@gmail.com>
2025-12-16 00:08:01 +0900

    Title: 살 수 있는 물건 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251216/살 수 있는 물건 2/things-able-to-buy-2.py
+++ 251216/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -1,10 +1,10 @@
 n = int(input())
 
-if n == 3000 :
+if n >= 3000 :
     print('book')
-elif n == 1000 :
+elif n >= 1000 :
     print('mask')
-elif n == 500 :
+elif n >= 500 :
     print('pen')
 else : 
     print('no')
