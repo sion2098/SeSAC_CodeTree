a5a50e885e1a7c470a4bba1d981f284c6a8c219d
sion2098 <sioning2098@gmail.com>
2025-12-17 01:00:21 +0900

    Title: 두 사람, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251217/두 사람/two-person.py
+++ 251217/두 사람/two-person.py
@@ -9,7 +9,7 @@ a_s = str(arr[1])
 b_a = int(arr2[0])
 b_s = str(arr2[1])
 
-if (a_a >= 19 or b_a >= 19) and (a_s == 'M' or b_s == 'M') :
+if (a_a >= 19 or b_a >= 19) or (a_s == 'M' or b_s == 'M') :
     print(1)
 else :
     print(0)
