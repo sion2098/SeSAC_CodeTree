ba6e665e8433dd36b7d5f2677b3b9c9454b2ff6b
sion2098 <sioning2098@gmail.com>
2025-12-17 00:55:00 +0900

    Title: 두 사람, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251217/두 사람/two-person.py
@@ -0,0 +1,15 @@
+inp = input()
+inp2 = input()
+arr = inp.split()
+arr2 = inp2.split()
+
+a_a = int(arr[0])
+a_s = str(arr[1])
+
+b_a = int(arr2[0])
+b_s = str(arr2[1])
+
+if (a_a >= 10 or b_a >= 19) and (a_s == 'M' or b_s == 'M') :
+    print(1)
+else :
+    pass
